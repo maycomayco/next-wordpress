77f146e4cb7f126a9c4db7665f8f7331fe887762
Mayco Barale <maycobarale@gmail.com>
2023-07-17 20:04:30 -0300

    Add config for remote images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'ceropixel.com.ar',
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig
